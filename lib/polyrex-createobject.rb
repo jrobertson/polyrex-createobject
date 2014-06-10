50d92605d0cf2a133458f20c0d237d9bb1e3c827
James Robertson <james@r0bertson.co.uk>
2014-06-10 20:37:45 +0100

    Refactored the code in preparation for a future feature.

--- lib/polyrex-createobject.rb
+++ lib/polyrex-createobject.rb
@@ -2,35 +2,50 @@
 
 # file: polyrex-createobject.rb
 
-require 'polyrex-schema'
+
 require 'rexle'
+require 'polyrex-schema'
 
 
 class PolyrexCreateObject
 
   attr_accessor :parent_node
-  
+  attr_reader :obj
+
+
   def initialize(schema, id='1')
+
     @@id = id
 
-    @schema = schema
+    raise "missing schema" unless schema
 
-    if @schema then
-      a = @schema.split('/')        
-      @rpaths = (a.length).times.inject({}) do |r| 
-        r.merge ({a.join('/').gsub(/\[[^\]]+\]/,'') => a.pop}) 
+    @schema = schema[/\/.*$/][1..-1]
+    a = PolyrexSchema.new(schema).to_a
+    @obj = attach_create_handlers(a)
+
+    @obj.class_eval do 
+      def record=(node)
+        @parent_node = node
       end
-
-      values = @rpaths.values.reverse
-
-      @rpaths.keys.reverse.each.with_index do |key,i |
-        @rpaths[key] = values[i..-1].join('/')
-      end
-
-      names = @rpaths.to_a[0..-2].map {|k,v| [v[/[^\[]+/], k]}
-      
-      attach_create_handlers(names)
     end
+
+    self.instance_eval " def #{@obj.name.downcase}(h, id=@@id, &blk)
+
+      new_parent = create_node(@parent_node, @schema, h, id).element('records')
+
+      obj = @obj.new
+      obj.record = new_parent
+      obj.instance_variable_set(:@schema, @schema[/\\/(.*$)/,1])
+      
+
+      if block_given? then
+        blk.call obj
+      else
+        obj
+      end
+    end
+    "    
+
   end
   
   def id=(s)  @@id = s; self end
@@ -40,51 +55,60 @@ class PolyrexCreateObject
     @parent_node = node
   end
 
-  def attach_create_handlers(names)
-    methodx = names[0..-2].map do |name, xpath|
+  private
 
-# nested records
-%Q(
-  def #{name}(params={}, id=nil,&blk) 
+  def attach_create_handlers(a)
 
-    id ||= @@id
-    orig_parent = @parent_node
-    new_parent = create_node(@parent_node, @rpaths['#{xpath}'], params, id).element('records')
-
-    if block_given? then
-      self.record = new_parent
-      blk.call(self) 
-    end
-
-    self.record = orig_parent
-
-    self
-  end
-)
-    end
-
-    name, xpath = names[-1]
-
-# top record    
-    methodx << %Q(
-def #{name}(params={}, id=nil,&blk)
-  id ||= @@id
-  orig_parent = @parent_node
-  self.record = @parent_node.element('records') unless @parent_node.name == 'records'
-  self.record = create_node(@parent_node, @rpaths['#{xpath}'], params, id).element('records')        
-  blk.call(self) if block_given?
-  self.record = orig_parent
-
-  self
-end
-)
-
-    self.instance_eval(methodx.join("\n"))
-    
+    class_name = "root".capitalize
+    parent_klass = Object.const_set(class_name,Class.new)
+    result = scan parent_klass, a
   end
 
   def create_node(parent_node, child_schema, params={}, id=nil)
 
+    buffer = PolyrexSchema.new(child_schema[/^[^\/]+/]).to_s
+    record = Rexle.new buffer     
+
+    if id then
+      @@id.succ!
+    else
+      if @@id.to_i.to_s == @@id.to_s then
+        @@id.succ!
+      else
+        @@id = @parent_node.element('count(//@id)').to_i + 2
+      end
+    end
+
+    record.root.add_attribute({'id' => @@id.to_s.clone})
+
+    a = child_schema[/[^\[]+(?=\])/].split(',')
+
+    summary = record.root.element('summary')
+    a.each do |field_name|  
+      field = summary.element(field_name.strip)
+      field.text = params[field_name.strip.to_sym]
+    end
+
+    parent_node.add record.root
+
+  end
+
+  def scan(parent, list)
+
+    record = list.shift
+    args = list
+
+    r = []
+
+    fields = []
+    fields << args.shift while args.first.is_a? Symbol
+
+    class_name = record.capitalize
+    klass = Object.const_set(class_name,Class.new)
+
+    parent.class_eval do
+
+      define_method :create_node do |parent_node, child_schema, params={}, id=nil|
 
     buffer = PolyrexSchema.new(child_schema[/^[^\/]+/]).to_s
     record = Rexle.new buffer     
@@ -114,8 +138,69 @@ end
   end
 
 
-  def xpath_to_rpath(xpath)
-    xpath.split('/').each_slice(2).map(&:last).join('/').gsub(/\[[^\]]+\]/,'')
+      define_method record do |h, id=nil, &blk|
+
+        id ||= @@id
+        local_schema = "%s[%s]" % [record, fields.join(',')]        
+        new_parent = create_node(@parent_node, local_schema, h, id).element('records')
+        
+        obj = klass.new
+
+        def obj.record=(node)
+          @parent_node = node
+        end
+
+
+        obj.record = new_parent
+        #if block_given? then
+        if blk then
+          blk.call obj  
+        else
+          obj
+        end
+        self
+      end
+    end
+
+    return klass unless args.any?
+
+    next_rec = args.shift
+
+    if next_rec.first.is_a? Array then
+      remaining = scan(parent, *args) unless args.length < 1
+
+      next_rec.each do |x| 
+
+        child_klass = scan(klass,x)
+
+        if remaining then
+
+          child_klass.class_eval do
+
+            define_method remaining.name.downcase.to_sym do |h, id=nil, &blk|
+
+              id ||= @@id
+              local_schema = "%s[%s]" % [record, fields.join(',')]
+
+              new_parent = create_node(@parent_node, local_schema, params, id).element('records')
+
+              obj = remaining.new
+              obj.record = new_parent
+              yield obj
+
+              self
+            end
+          end
+        end
+      end
+
+    else
+      remaining = scan(klass, *list) unless args.length < 1
+      scan(klass, next_rec)
+      scan(r, remaining) if remaining
+    end
+
+    return klass
   end
 
 end
--- polyrex-createobject.gemspec
+++ polyrex-createobject.gemspec
@@ -1,13 +1,14 @@
 Gem::Specification.new do |s|
   s.name = 'polyrex-createobject'
-  s.version = '0.4.15'
+  s.version = '0.4.17'
   s.summary = 'polyrex-createobject'
   s.authors = ['James Robertson']
   s.files = Dir['lib/**/*.rb']
-  s.add_dependency('polyrex-schema') 
+  s.add_runtime_dependency('polyrex-schema', '~> 0.1', '>=0.1.16') 
   s.signing_key = '../privatekeys/polyrex-createobject.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
   s.email = 'james@r0bertson.co.uk'
   s.homepage = 'https://github.com/jrobertson/polyrex-createobject'
+  s.required_ruby_version = '>= 2.1.2'
 end
